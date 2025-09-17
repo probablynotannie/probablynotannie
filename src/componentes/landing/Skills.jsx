bf3ddd46a50ac6651f6dfbe7016e06bfb8a0e8d4
Ana <probablynotannie@gmail.com>
2025-09-17 13:27:29 +0200

    Arreglado

--- src/componentes/landing/Skills.jsx
+++ src/componentes/landing/Skills.jsx
@@ -11,39 +11,9 @@ function Skills() {
     },
     {
       id: 1,
-      texto: "Tailwind css",
+      texto: "Tailwind CSS",
       svg: (
-        <span className="text-4xl">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 256 256"
-          >
-            <g fill="none">
-              <rect width="350" height="265" fill="#242938" rx="60" />
-              <path
-                fill="url(#skillIconsTailwindcssDark0)"
-                fillRule="evenodd"
-                d="M83 110q9-36 45-36c36 0 40.5 27 58.5 31.5q18 4.502 31.5-13.5q-9 36-45 36c-36 0-40.5-27-58.5-31.5Q96.5 92 83 110m-45 54q9-36 45-36c36 0 40.5 27 58.5 31.5q18 4.502 31.5-13.5q-9 36-45 36c-36 0-40.5-27-58.5-31.5q-18-4.502-31.5 13.5"
-                clipRule="evenodd"
-              />
-              <defs>
-                <linearGradient
-                  id="skillIconsTailwindcssDark0"
-                  x1="86.5"
-                  x2="163.5"
-                  y1="74"
-                  y2="185.5"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#32B1C1" />
-                  <stop offset="1" stopColor="#14C6B7" />
-                </linearGradient>
-              </defs>
-            </g>
-          </svg>{" "}
-        </span>
+        <img src="/skills/tailwind.png" className="w-10 h-8 object-contain" />
       ),
     },
     {
@@ -53,8 +23,8 @@ function Skills() {
     },
     {
       id: 3,
-      texto: "Zustand",
-      svg: <img src="/skills/zustand.png" className="w-10" />,
+      texto: "React Router",
+      svg: <img src="/skills/reactRouter.svg" className="w-10" />,
     },
     {
       id: 6,
@@ -150,8 +120,8 @@ function Skills() {
     },
     {
       id: 13,
-      texto: "flowbite",
-      svg: <img src="/skills/flowbite.png" className="w-10" />,
+      texto: "Zustand",
+      svg: <img src="/skills/zustand.png" className="w-10" />,
     },
     {
       id: 14,
