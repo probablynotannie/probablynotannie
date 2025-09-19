da9f38bff47102f71125403d7814e722ad034fff
Ana <probablynotannie@gmail.com>
2025-09-19 23:41:00 +0200

    overflow

--- src/componentes/body/Particles.jsx
+++ src/componentes/body/Particles.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Particulos() {
   return (
-    <div className="">
+    <div className="overflow-x-hidden">
       <div className="floating-orb w-96 h-96 bg-gradient-to-r from-purple-400 to-indigo-400 top-10 -left-48 opacity-20"></div>
       <div className="floating-orb-2 w-80 h-80 bg-gradient-to-r from-pink-400 to-pink-500 top-10 -right-48 opacity-20"></div>
       <div className="floating-orb w-28 h-28 bg-gradient-to-r from-blue-400 to-cyan-400 top-10 right-1/3 opacity-20"></div>
